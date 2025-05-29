c50547026eaf43111928ae695c672830047ea741
bbstar8 <boluwatifeayesa6@gmail.com>
2025-05-29 11:15:19 +0100

    feat:added how-to-play-modal functionality

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loader = document.getElementById('loader');
     const loadingText = loader.querySelector('.loading-text');
+    const howToPlayModal = document.getElementById('how-to-play-modal');
 
     let percent = 0;
     const interval = setInterval(()=>{
@@ -9,7 +10,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (percent >= 100){
             clearInterval(interval);
-            setTimeout(()=>{loader.classList.add('hidden');}, 300);
+            setTimeout(()=>{
+                loader.classList.add('hidden');
+            howToPlayModal.classList.remove('hidden');
+        howToPlayModal.classList.add('flex');
+    }, 300);
         }
-    }, 30)
-})
+    }, 30);
+
+    // Proceed button
+    document.getElementById('proceed-btn').addEventListener('click', () => {
+    howToPlayModal.classList.remove('flex');
+    howToPlayModal.add('hidden');
+});
+});
+
+
+
